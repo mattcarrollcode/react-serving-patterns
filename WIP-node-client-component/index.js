a7c4310dca1e050e54fe06792e68d2c3c46cda74
Matt Carroll <mattca@meta.com>
2023-07-12 11:05:12 -0700

    Document webpack issue

--- WIP-node-client-component/index.js
+++ WIP-node-client-component/index.js
@@ -1,4 +1,7 @@
 const register = require("react-server-dom-webpack/node-register");
+// register() needs to be called before other imports
+// so that React can parse the "use client" directive from
+// module imports
 register();
 
 const React = require("react");
@@ -24,6 +27,17 @@ app.get("/rsc", (request, response) => {
     React.createElement(ServerComponent, null),
     React.createElement(ClientComponent, null)
   );
+  // react-dom.development.js:25109 Uncaught ReferenceError: __webpack_require__ is not defined
+  //   at requireModule (react-server-dom-webpack-client.browser.development.js:137:25)
+  //   at initializeModuleChunk (react-server-dom-webpack-client.browser.development.js:1166:19)
+  //   at readChunk (react-server-dom-webpack-client.browser.development.js:991:9)
+  //   at mountLazyComponent (react-dom.development.js:15608:21)
+  //   at beginWork$1 (react-dom.development.js:17319:18)
+  //   at beginWork (react-dom.development.js:25692:16)
+  //   at performUnitOfWork (react-dom.development.js:24543:14)
+  //   at workLoopConcurrent (react-dom.development.js:24529:7)
+  //   at renderRootConcurrent (react-dom.development.js:24485:11)
+  //   at performConcurrentWorkOnRoot (react-dom.development.js:23342:40)
   const clientComponentManifest = {
     [`file://${__dirname}/client-component.js`]: {
       id: `/client-component.js`,
