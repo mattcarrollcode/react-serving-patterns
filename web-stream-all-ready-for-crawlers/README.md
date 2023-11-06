07e61280cff42ebdd04624da7c646e5d00a45e13
Matt Carroll <mattca@meta.com>
2023-11-06 15:16:31 -0800

    update allReady web stream example

--- web-stream-all-ready-for-crawlers/README.md
+++ web-stream-all-ready-for-crawlers/README.md
@@ -1,6 +1,8 @@
-
 Prereqs
-1. install Deno: https://docs.deno.com/runtime/manual/getting_started/installation
+
+1. install Deno:
+   https://docs.deno.com/runtime/manual/getting_started/installation
 
 Run
-1. `deno run index.js`
+
+1. `deno run --allow-all --watch index.jsx`
--- web-stream-all-ready-for-crawlers/index.jsx
+++ web-stream-all-ready-for-crawlers/index.jsx
@@ -1,14 +1,47 @@
-import React from "npm:react@0.0.0-experimental-ce2bc58a9-20231102"
+import React from "npm:react@0.0.0-experimental-ce2bc58a9-20231102";
 import { renderToReadableStream } from "npm:react-dom@0.0.0-experimental-ce2bc58a9-20231102/server";
+const { use, useState, Suspense } = React;
 
-async function handler() {
-    const stream = await renderToReadableStream(<h1>Hi!</h1>, {});
-    await stream.allReady;
-    return new Response(stream, {
-        status: 200,
-        headers: { 'content-type': 'text/html' },
-    })
+function fetchMessage(delay) {
+  return new Promise((resolve) => setTimeout(resolve, delay, "React"));
 }
 
-const response = await handler()
-console.log(await response.text())
+function Message({ messagePromise }) {
+  const messageContent = React.use(messagePromise);
+  return <p>Here is the message: {messageContent}</p>;
+}
+
+function App() {
+  const [messagePromise1] = useState(() => fetchMessage(1000));
+  const [messagePromise2] = useState(() => fetchMessage(2000));
+  const [messagePromise3] = useState(() => fetchMessage(3000));
+  return (
+    <>
+      <h1>renderToReadableStream allReady example</h1>
+      <Suspense fallback={<p>⌛Downloading 1st message...</p>}>
+        <Message messagePromise={messagePromise1} />
+      </Suspense>
+      <Suspense fallback={<p>⌛Downloading 2nd message...</p>}>
+        <Message messagePromise={messagePromise2} />
+      </Suspense>
+      <Suspense fallback={<p>⌛Downloading 3rd message...</p>}>
+        <Message messagePromise={messagePromise3} />
+      </Suspense>
+    </>
+  );
+}
+
+async function handler() {
+  console.time("Time for stream to start");
+  const stream = await renderToReadableStream(<App />, {});
+  console.timeEnd("Time for stream to start");
+  console.time("Time waiting for stream to be ready");
+  await stream.allReady;
+  console.timeEnd("Time waiting for stream to be ready");
+  return new Response(stream, {
+    status: 200,
+    headers: { "content-type": "text/html" },
+  });
+}
+
+Deno.serve({ port: 3000 }, handler);
